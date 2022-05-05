763288e505da726d011b592d2bded1840c5f35c4
i-Taylo <78148850+i-Taylo@users.noreply.github.com>
2022-05-05 18:04:18 +0300

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,77 @@
+# Installer
+SKIPUNZIP=1
+TERMUX=1
+CALLBACK=6
+unzip -qo "$ZIPFILE" "function.sh" "$TMPDIR"
+. "$TMPDIR/function.sh"
+dezip "$ZIPFILE" "system.prop" "$MODPATH"
+dezip "$ZIPFILE" "system.prop" "$MAINPATH"
+dezip "$ZIPFILE" "system/*" "$MODPATH"
+dezip "$ZIPFILE" "system/*" "$MAINPATH"
+dezip "$ZIPFILE" "lib-daemon" "$MODPATH"
+dezip "$ZIPFILE" "clean" "$MODPATH/system/bin"
+dezip "$ZIPFILE" "lib-iunlocker" "$MODPATH"
+dezip "$ZIPFILE" "iUnlocker-Sapphire" "$MODPATH"
+dezip "$ZIPFILE" "customize.sh" "$MODPATH"
+dezip "$ZIPFILE" "iUnlocker-fuse" "$MODPATH"
+dezip "$ZIPFILE" "module.prop" "$MAINPATH"
+dezip "$ZIPFILE" "module.prop" "$MODPATH"
+dezip "$ZIPFILE" "lib-taylo" "$MODPATH"
+dezip "$ZIPFILE" "uninstaller" "$MODPATH"
+dezip "$ZIPFILE" "lib-check" "$MODPATH"
+dezip "$ZIPFILE" "post-fs-data.sh" "$MODPATH"
+dezip "$ZIPFILE" "iUnlocker-ES.bin" "$MODPATH"
+dezip "$ZIPFILE" "post-fs-data.sh" "$MAINPATH"
+dezip "$ZIPFILE" "service.sh" "$MODPATH"
+dezip "$ZIPFILE" "uninstall.sh" "$MODPATH"
+dezip "$ZIPFILE" "uninstaller" "$MODPATH"
+set_perm $MODPATH/lib-check
+run lib-check
+if [ -f $MODPATH/out ]; then
+abort
+fi
+unzip -qo "$ZIPFILE" "files/$ABI" "$TMPDIR"
+if [ "$ARCH" = "arm" ] ; then
+  unzip -qo "$ZIPFILE" "files/$ARCH/*" -d "$MODPATH"; mv $MODPATH/files/$ARCH/* $MODPATH/system/bin; rm -rf $MODPATH/files
+elif [ "$ARCH" = "arm64" ]; then
+  unzip -qo "$ZIPFILE" "files/$ARCH/*" -d "$MODPATH"; mv $MODPATH/files/$ARCH/* $MODPATH/system/bin; rm -rf $MODPATH/files
+elif [ "$ARCH" = "x86" ]; then
+  unzip -qo "$ZIPFILE" "files/$ARCH/*" -d "$MODPATH"; mv $MODPATH/files/$ARCH/* $MODPATH/system/bin; rm -rf $MODPATH/files
+elif [ "$ARCH" = "x64" ]; then
+  unzip -qo "$ZIPFILE" "files/$ARCH/*" -d "$MODPATH"; mv $MODPATH/files/$ARCH/* $MODPATH/system/bin; rm -rf $MODPATH/files
+fi
+mv $MODPATH/system/bin/busybox $MODPATH/system/bin/wget
+
+     if [ "$MIN_ANDROID_API" = "$API" ]; then
+      cout "Android version: $API\n"
+      elif [ "$MAX_ANDROID_API" = "$API" ]; then
+       cout "Android version: $API\n"
+      elif [ "$API" = 31 ]; then
+       cout "Android version: $API\n"
+       else
+      cout_abort "unsupported android version $API\n"
+    fi
+cout "Using: $ARCH,ON: $ABI,SDK: $API\n"
+chmod 777 $MODPATH/*
+config path=/../../system-patch
+config proc=$MODDIR
+config infinity=512
+config JUNK=null
+config IS64=$IS64BIT
+config Arch=$ARCH
+config ROOT_VER=$MAGISK_VER
+config SDM=yes
+config pROOT=false
+config fps_limt=144fps
+config FRAME=2400x1080
+config isresMAX=no
+config ADRENO='Adreno (TM) 660'
+cp -af $MODPATH/system/bin/wget /system/bin
+chmod 777 /system/bin/wget
+fstop com.termux
+TERMUXL; HTP; HSR $CEV
+run iUnlocker-Sapphire
+chmod 777 $MODPATH/system/bin/*
+chmod 777 $MODPATH/*
+set_perm $MODPATH/iUnlocker-Sapphire
+chmod 777 $MODPATH/lib-iunlocker
---
+++ function.sh
@@ -0,0 +1,32 @@
+#!/system/bin/sh
+MAINPATH="/data/adb/modules/iUnlocker-Sapphire"
+dezip() { unzip -qo "$1" "$2" -d "$3" | echo "ZIPFILE: [☄: $2)"; }
+progress() { sleep "$@"; }
+set_perm() { chmod 777 "$@"; }
+cout() { printf "PROCESS: [☄: $1"; }
+GETPROP() { grep "^$2" "$1" | head -n1 | cut -d= -f2-; }
+rename() { mv "$@" "$@"; }
+config() { echo "$1" >> "/data/adb/modules_update/iUnlocker-Sapphire/doze.ini"; }
+run() { su -c "/data/adb/modules_update/iUnlocker-Sapphire/$1"; }
+TERMUXL() { monkey -p com.termux -c android.intent.category.LAUNCHER 1 > /dev/null 2>&1; }
+set_per() { chmod 777 "$@"; }
+HTP() { input text "su -c /data/adb/modules_update/iUnlocker-Sapphire/lib-iunlocker"; }
+HSR() { input keyevent "$@"; }
+ints() { am start -a $@ -d $@ > /dev/null 2>&1; }
+fstop() {  am force-stop $@  > /dev/null 2>&1; }
+cout_abort() {
+  cout "$1"
+  $BOOTMODE || recovery_cleanup
+  [ ! -z $MODPATH ] && rm -rf $MODPATH
+  rm -rf $TMPDIR
+  exit 1
+}
+
+MIN_ANDROID_VER=11
+MIN_ANDROID_API=30
+MIN_ANDROID_VER_CODE=30000
+MIN_ANDROID_VER_NAME=Honeycomb
+MAX_ANDROID_VER=12
+MAX_ANDROID_API=32
+CEV=KEYCODE_ENTER
+MAX_ANDROID_CODE=12000
